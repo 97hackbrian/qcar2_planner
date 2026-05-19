386532c587314db7438c08c3fa1e8c0ce51a46de
97hackbrian <brayandurantoconas@gmail.com>
2026-05-19 18:16:06 -0400

    chore: tune cartographer pose publish period, scan matcher weights, and motion filter settings

--- config/cartographer_2d.lua
+++ config/cartographer_2d.lua
@@ -19,7 +19,7 @@ options = {
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
-  pose_publish_period_sec = 5e-3,
+  pose_publish_period_sec = 50e-3,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
@@ -40,20 +40,20 @@ TRAJECTORY_BUILDER_2D.use_imu_data = true    -- ENABLE IMU FUSION
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.2
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(30.)
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1e-1
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 1e-1
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 5e-1
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 5e-1
 
 -- Ceres scan matcher tuning for robust tracking
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.occupied_space_weight = 1.
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10.
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 40.
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 40.
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 100.
 
 -- Submaps
 TRAJECTORY_BUILDER_2D.submaps.num_range_data = 90
 TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.05
 
 -- Motion filter
-TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 0.5
+TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 0.3 --#0.1
 TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.9
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.2)
 
